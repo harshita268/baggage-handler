2cc17f95ca565db94748f34e8dd9bec1c4377572
Harshita <Harshita>
2018-04-15 17:32:12 +0530

    FIX: timer

--- static/js/baggage-checkin.js
+++ static/js/baggage-checkin.js
@@ -1,9 +1,8 @@
 /*Baggage checkin simulation goes here.*/
 
 
-var _data;
+var _data, _status = 'stop', count = create_default_time();
 var visualize_checkin = function(obj) {
-	console.log(_data)
 	for(var i=0;i<50;i++){
 		$('.counter-1').append(
 			"<div class='passenger' title='passenger " + (i+1) + "'></div>")
@@ -13,6 +12,7 @@ var visualize_checkin = function(obj) {
 var baggage_scanner = function() {
 
 }
+
 
 var add_seats = function() {
 	var obj = {
@@ -49,46 +49,6 @@ var add_seats = function() {
 }
 
 var add_baggage_bins = function() {
-	// 	var obj = {
-	// 	"seat_size": 5.6,
-	// 	"max_seats": 384
-	// }
-	// var bins = {
-	// 	"0" : '#123',
-	// 	"1" : '#ccc',
-	// 	"2" : "#555",
-	// 	"3" : "#333"
-	// }
-	// var curr_y = 612., curr_x = 215., offset_passage = 0.0, bin = 0;
-	// var fuselage = d3.selectAll('#plane1 #fuselage');
-	// 	fuselage.selectAll('rect')
-	// 		.data(d3.range(0, obj.max_seats))
-	// 		.enter()
-	// 		 .append('rect')
-	// 		 .attr("width", obj.seat_size)
-	// 		 .attr("class", function(i,d) { return 'seat-' + i })
-	// 		 .attr("height", obj.seat_size)
-	// 		 .attr("fill", function(d){
-	// 		 		bin = Math.floor(d / 96)
-	// 		 		return bins[bin]
-	// 		 	})
-	// 		 .attr("x", function(i, d) {
-	// 			if(i % 4 == 0) {
-	// 			 	offset_passage = 2.;
-	// 			}
-	// 			if(i % 8 == 0) {
-	// 				offset_passage = 0.0
-	// 			}
-	// 		 	return curr_x + offset_passage + ((i % 8) * obj.seat_size)
-	// 		 })
-	// 		 .attr("y", function(i, d) {
-	// 		 	if(i % 8 == 0) {
-	// 		 		curr_y += obj.seat_size + 0.3;
-	// 		 		//curr_x = 215.;
-	// 		 	}
-
-	// 			return curr_y;
-	// 		})
 	$.get('static/svg/baggage.txt', function(d){
 		d3.select('#plane1 #fuselage')
 			.html(d)
@@ -98,4 +58,46 @@ var add_baggage_bins = function() {
 
 var conveyor = function() {
 
+}
+
+
+var _click_handlers = function(node) {
+	status = node.attr('status');
+	if(status == 'play') {
+		node.removeClass('glyphicon-play');
+		node.addClass('glyphicon-pause');
+		node.attr('status', 'pause')
+		$('.glyphicon-stop').removeClass('disabled', 'false')
+	}
+	else if(status == 'pause') {
+		node.removeClass('glyphicon-pause');
+		node.addClass('glyphicon-play');
+		node.attr('status', 'play')
+		$('.glyphicon-stop').removeClass('disabled', 'false')
+	}
+	else if(status == 'stop') {
+		$('.glyphicon-stop').addClass('disabled', 'true')
+		$('.glyphicon-pause, .glyphicon-play').removeClass('glyphicon-play')
+		$('.glyphicon-pause, .glyphicon-play').removeClass('glyphicon-pause')
+		$('.glyphicon').eq(0).addClass('glyphicon-play')
+		$('.glyphicon').eq(0).attr('status', 'play')
+		count = create_default_time()
+		$('t').text('__:__:__');
+	}
+}
+
+setInterval(myMethod, 1000);
+function myMethod()
+{
+  if(status == 'play') {
+	count += 1;
+	var measuredTime = new Date(null);
+	measuredTime.setSeconds(count); // specify value of SECONDS
+	var MHSTime = measuredTime.toISOString().substr(11, 8);
+	$('t').text(MHSTime);
+  }
+}
+
+function create_default_time() {
+	return d3.min(_data, function(c) { return c['Time'] }).split(':').reduce((acc,time) => (60 * acc) + +time)
 }
