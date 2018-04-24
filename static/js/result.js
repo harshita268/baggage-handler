54c5bbdc088db9970d332bdf201a414494c77435
Harshita <harshita.prajapati99@gmail.com>
2018-04-24 07:39:48 +0530

    COM: result page

--- static/js/result.js
+++ static/js/result.js
@@ -88,12 +88,13 @@ function grouped_bar() {
 
 
 
-function timeline() {
-  var testData = [
-        {times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
-        {times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
-        {times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
-      ];
+function timeline(data) {
+  // var testData = [
+  //       {times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+  //       {times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
+  //       {times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+  //     ];
+  var testData = data;
 
       
       var width = 500;
